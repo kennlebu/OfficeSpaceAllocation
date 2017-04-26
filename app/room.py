c87ef1da4873999d2c3b4c825e9f858978eabd6c
Kenneth Lebu <kenlebu@gmail.com>
2017-04-26 14:20:32 +0300

    modified room to store names in upper case

--- app/room.py
+++ app/room.py
@@ -2,6 +2,6 @@ class Room():
     """ Defines a room in the Dojo. """
 
     def __init__(self, room_name):
-        self.room_name = room_name
-        self.occupants = 0  # Number of people in the room currently
+        self.room_name = room_name.upper()
+        #self.occupants = []  # People in the room currently
 
