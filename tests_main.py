d28e810cd9931cde7fc6af94337356d4b8691fb0
Kenneth Lebu <kenlebu@gmail.com>
2017-04-28 02:37:05 +0300

    added main method to call tests more easily

--- tests_main.py
+++ tests_main.py
@@ -11,13 +11,11 @@ import nose
 import coverage
 import os
 
-
-if __name__ == '__main__':
-    #unittest.main()
-    #nose.run(argv=['--with-coverage'])
+def main():    
     file_path = os.path.abspath(__file__)
     tests_path = os.path.join(os.path.abspath(os.path.dirname(file_path)), "tests")
     result = nose.run(argv=[os.path.abspath(__file__),
                             "--with-cov", "--verbosity=3", "--cover-package=app", tests_path])
 
-
+if __name__ == '__main__':
+    main()
