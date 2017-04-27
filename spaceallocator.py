f94ac5a5351d31ca0761bc48bc36c3ff795abbf8
Kenneth Lebu <kenlebu@gmail.com>
2017-04-27 14:17:24 +0300

    removed errors preventing docopt from running

--- spaceallocator.py
+++ spaceallocator.py
@@ -1,60 +1,67 @@
 """Office Space Allocator.
 
 Usage:
-  spaceallocator create_room <room_type> <room_name>...
-  spaceallocator add_person <person_name> <FELLOW|STAFF> [wants_accommodation]
-  spaceallocator print_room <room_name>
-  spaceallocator print_allocations [-o=filename]
-  spaceallocator print_unallocated [-o=filename]
-  spaceallocator reallocate_person <person_identifier> <new_room_name>
-  spaceallocator load_people <filename>
-  spaceallocator save_state [--db=sqlite_database]
-  spaceallocator load_state <sqlite_database>
-  spaceallocator (-h | --help)
-  spaceallocator (-i | --interactive)
-  spaceallocator --version
+    spaceallocator create_room <room_type> <room_name>...
+    spaceallocator add_person <person_name> <FELLOW|STAFF> [wants_accommodation]
+    spaceallocator print_room <room_name>
+    spaceallocator print_allocations [-o=filename]
+    spaceallocator print_unallocated [-o=filename]
+    spaceallocator reallocate_person <person_identifier> <new_room_name>
+    spaceallocator load_people <filename>
+    spaceallocator save_state [--db=sqlite_database]
+    spaceallocator load_state <sqlite_database>
+    spaceallocator (-h | --help)
+    spaceallocator (-i | --interactive)
+    spaceallocator --version
 
 Options:
-  -h --help     Sorry mate, no help for you.
-  -v --version     Show version.
-  -i --interactive      Interactive Mode
+    -h --help    Sorry mate, no help for you.
+    -v --version    Show version.
+    -i --interactive    Interactive Mode
 """
 
 import cmd
 import sys
 from docopt import docopt, DocoptExit
-from termcolor import cprint
+from termcolor import colored, cprint
+from pyfiglet import figlet_format
 from app.dojo import Dojo
 
-def docopt_cmd(prompt):
-    """ Thus decorator is used to simplify the try/except block and pass
-    the result of the decopt parsing to the called action.
+def docopt_cmd(func):
     """
-
-    def fun(self, args):
+    This decorator is used to simplify the try/except block and pass the result
+    of the docopt parsing to the called action.
+    """
+    def fn(self, arg):
         try:
-            opt = docopt(fun.__doc__, args)
-        except DocoptExit as sys_exit:
-            # This is thrown when the args don't match
-            cprint('That command is not recognized', 'Red' 'on_grey')
-            print(sys_exit)
+            opt = docopt(fn.__doc__, arg)
+
+        except DocoptExit as e:
+            # The DocoptExit is thrown when the args do not match.
+            # We print a message to the user and the usage block.
+
+            print('Invalid Command!')
+            print(e)
             return
 
         except SystemExit:
-            # Prints the usage for --help
+            # The SystemExit exception prints the usage for --help
+            # We do not need to do the print here.
+
             return
 
-        return prompt(self, opt)
+        return func(self, opt)
 
-    fun.__name__ = fun.__name__
-    fun.__doc__ = fun.__doc__
-    fun.__dict__.update(prompt.__dict__)
-    return fun
+    fn.__name__ = func.__name__
+    fn.__doc__ = func.__doc__
+    fn.__dict__.update(func.__dict__)
+    return fn
 
 
 class InteractiveShell(cmd.Cmd):
-    cprint("Welcome to Office Space Allocator", 'cyan', attrs=['bold'])
-    cprint("Type -h or --help for a list of commands", 'cyan')
+    print(colored(figlet_format('Office Space Allocator', font='contrast'), 'cyan',
+                  attrs=['bold']))
+    intro = 'Welcome to Office Space Allocator \nType help to get a list of commands to use'
 
     prompt = '(Space Allocator) '
 
@@ -97,12 +104,12 @@ class InteractiveShell(cmd.Cmd):
         """ Usage: load_people <filename> """
         print(args)
 
-    @docopt
+    @docopt_cmd
     def do_save_state(self, args):
         """ Usage: save_state [--db=sqlite_database] """
         print(args)
 
-    @docopt
+    @docopt_cmd
     def do_load_state(self, args):
         """ Usage: load_state <sqlite_database> """
         print(args)
